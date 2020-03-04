6be15826b9a07e51ab5e8fbfc9a81b5fcdb8f9c9
Alex <xandiem@yahoo.co.uk>
2020-03-04 21:46:27 +0000

    Significant additions: Robot added and placed correctly Joystick functionality added- Some bugs with x and z coordinate at present JSON file for storing the simulation scenario in- Rooms stored here at present

--- Python1.py
+++ Python1.py
@@ -1,14 +1,17 @@
 from os.path import dirname, join, abspath
 from pyrep import PyRep
+from pyrep.robots.mobiles.youbot import YouBot
 from pyrep.objects.shape import Shape
 from pyrep.const import PrimitiveShape
 from random import randint
+import numpy as np
 import json
 import os
 import sys    
 import termios
 import fcntl
 import time
+import pygame
 
 #setup CoppeliaSim and launch the scene file
 SCENE_FILE = join(dirname(abspath(__file__)), 'scene.ttt')
@@ -16,182 +19,190 @@ pr = PyRep()
 pr.launch(SCENE_FILE, headless=False) 
 pr.start()  # Start the simulation
 
-#rectangle method for room generation 
+agent = YouBot()
+HEIGHT = 0.1
+
+#joystick setup
+pygame.joystick.init()
+joystick = pygame.joystick.Joystick(0)
+joystick.init()
+pygame.init()
+help(agent)
+def joystick_movement():
+	axes = joystick.get_numaxes()
+	for event in pygame.event.get():
+		pos_list = []
+		pos_rearranged = []
+		if event.type == pygame.JOYAXISMOTION:
+			for i in range(axes-1):
+				axis = joystick.get_axis(i)
+				value = axis * (5)
+				pos_list.append(value)
+				print(axis)
+				#rearrange axes as wrong order for function
+			pos_rearranged.append(pos_list[1])
+			pos_rearranged.append(-(pos_list[0]))
+			pos_rearranged.append(pos_list[2])
+			print(pos_rearranged)
+			agent.set_base_angular_velocites(pos_rearranged)
+def robot_set_position():	
+	starting_pose = agent.get_2d_pose()
+	agent.set_2d_pose(starting_pose)
+	position_min, position_max = [0.5, 1.0, 0.1], [-0.5, -1.0, 0.1]
+	pos = list(np.random.uniform(position_min, position_max))
+	agent.set_position(pos)
 def rectangular_room_generation():
 	#generate random to insert into the shape generation	
-	value_x = randint(30,80)/10
-	value_y = randint(30,80)/10
+	value_x = randint(40,80)/10
+	value_y = randint(40,80)/10
 	#store set of points
-	#top_cuboid = [(-value_x/2,2.0,2.0), (value_x/2,2.0,2.0), (-value_x/2,2.0, 0.0), 				(value_x/2,2.0,0.0)]
 	top = Shape.create(type=PrimitiveShape.CUBOID,
-                     size=[value_x, 0.05, 2.0],
+                     size=[value_x, 0.05, HEIGHT],
                      color=[0.8, 0.8, 0.8],
-		      		 position=[0.0, 2.0, 1.0],
-                     static=True, respondable=False)
-	#left_cuboid = [(-value_x/2,2.0,2.0), (-value_x/2,2.0-(value_y),2.0), (-value_x/2,2.0, 				0.0), (-value_x/2,2.0-(value_y),0.0)]
+		     position=[0.0, 2.0, HEIGHT/2],
+                     static=True, respondable=True)
 	left = Shape.create(type=PrimitiveShape.CUBOID,
-	              		  size=[0.05, value_y, 2.0],
-                  		color=[0.8, 0.8, 0.8],
-		      			  		position=[-value_x/2, 2-(value_y/2), 1.0],
-                      static=True, respondable=False)
-	#right_cuboid = [(value_x/2,2.0,2.0), (value_x/2,2.0-(value_y),2.0), (value_x/2,2.0, 				0.0), (value_x/2,2.0-(value_y),0.0)]
+		     size=[0.05, value_y, HEIGHT],
+                     color=[0.8, 0.8, 0.8],
+		     position=[-value_x/2, 2-(value_y/2), HEIGHT/2],
+                     static=True, respondable=True)
 	right = Shape.create(type=PrimitiveShape.CUBOID,
-                       size=[0.05, value_y, 2.0],
-                       color=[0.8, 0.8, 0.8],
-		                 	 position=[value_x/2, 2-(value_y/2), 1.0],
-                       static=True, respondable=False)
-	#bottom_cuboid = [(-value_x/2,2.0-(value_y),2.0), (value_x/2,2-(value_y),2.0), 				(-value_x/2,2.0-(value_y),0.0), (value_x/2,2-(value_y),0.0)]
+                     size=[0.05, value_y, HEIGHT],
+                     color=[0.8, 0.8, 0.8],
+		     position=[value_x/2, 2-(value_y/2), HEIGHT/2],
+                     static=True, respondable=True)
 	bottom = Shape.create(type=PrimitiveShape.CUBOID,
-                      	size=[value_x, 0.05, 2.0],
-                      	color=[0.8, 0.8, 0.8],
-		      							position=[0.0, 2-(value_y), 1.0],
-                      	static=True, respondable=False)
-	print("value_x", value_x)
-	print("valuey: ", value_y)
+                     size=[value_x, 0.05, HEIGHT],
+                     color=[0.8, 0.8, 0.8],
+		     position=[0.0, 2-(value_y), HEIGHT/2],
+                     static=True, respondable=True)
+	#call set robot to put robot in the room
+	robot_set_position()
 	data = {
-		'shape': 'lroom',
-    	'point1': (value_x/2, 2.0),
-    	'point2': (-value_x/2, 2.0),
-    	'point3': (-value_x/2, 2-value_y),
-    	'point4': (-value_x/2, 2-value_y)
+		'shape': 'rectangleroom',
+    		'point1': (value_x/2, 2.0),
+    		'point2': (-value_x/2, 2.0),
+    		'point3': (-value_x/2, 2-value_y),
+    		'point4': (-value_x/2, 2-value_y)
 	}
 	with open('data.json', 'a') as f:
 		json.dump(data, f)
-	#shape = [(value_x/2, 2.0), (-value_x/2, 2.0), (-value_x/2, 2-value_y), 
-	#		(-value_x/2, 2-value_y)]
-	#print(shape)
-	print(data)
 	
 def t_room_generation():
 	#generate random to insert into the shape generation
-	rand = randint(30,80)/10
-	#print("troom: ", rand + "")
+	rand = randint(40,80)/10
 	top = Shape.create(type=PrimitiveShape.CUBOID,
-                      size=[rand, 0.05, 2.0],
+                      size=[rand, 0.05, HEIGHT],
                       color=[0.8, 0.8, 0.8],
-		      		  position=[0.0, 2.0, 1.0],
-                      static=True, respondable=False)
+		      position=[0.0, 2.0, HEIGHT/2],
+                      static=True, respondable=True)
 	top_left = Shape.create(type=PrimitiveShape.CUBOID,
-	              			size=[0.05, rand/2, 2.0],
-                      		color=[0.8, 0.8, 0.8],
-		      				position=[-rand/2, 2-(rand/4), 1.0],
-                      		static=True, respondable=False)
+	              size=[0.05, rand/2, HEIGHT],
+                      color=[0.8, 0.8, 0.8],
+		      position=[-rand/2, 2-(rand/4), HEIGHT/2],
+                      static=True, respondable=True)
 	top_right = Shape.create(type=PrimitiveShape.CUBOID,
-                      		 size=[0.05, rand/2, 2.0],
-                      		 color=[0.8, 0.8, 0.8],
-		      								 position=[rand/2,2-(rand/4), 1.0],
-                      		 static=True, respondable=False)
+                      size=[0.05, rand/2, HEIGHT],
+                      color=[0.8, 0.8, 0.8],
+		      position=[rand/2,2-(rand/4), HEIGHT/2],
+                      static=True, respondable=True)
 	middle_left = Shape.create(type=PrimitiveShape.CUBOID,
-                      			 size=[rand/3, 0.05, 2.0],
-                      			 color=[0.8, 0.8, 0.8],
-		      									 position=[-(rand/3), 2-(rand/2), 1.0],
-                      			 static=True, respondable=False)
+                      size=[rand/3, 0.05, HEIGHT],
+                      color=[0.8, 0.8, 0.8],
+		      position=[-(rand/3), 2-(rand/2), HEIGHT/2],
+                      static=True, respondable=True)
 	middle_right = Shape.create(type=PrimitiveShape.CUBOID,
-                      				size=[rand/3, 0.05, 2.0],
-                      				color=[0.8, 0.8, 0.8],
-		      						position=[rand/3, 2-(rand/2), 1.0],
-                      				static=True, respondable=False)
+                      size=[rand/3, 0.05, HEIGHT],
+                      color=[0.8, 0.8, 0.8],
+		      position=[rand/3, 2-(rand/2), HEIGHT/2],
+                      static=True, respondable=True)
 	bottom_left = Shape.create(type=PrimitiveShape.CUBOID,
-	              				size=[0.05, rand/2, 2.0],
-                      			color=[0.8, 0.8, 0.8],
-		      					position=[-rand/2+(rand/3), 2-(0.75*rand), 1.0],
-                      			static=True, respondable=False)
+	              size=[0.05, rand/2, HEIGHT],
+                      color=[0.8, 0.8, 0.8],
+		      position=[-rand/2+(rand/3), 2-(0.75*rand), HEIGHT/2],
+                      static=True, respondable=True)
 	bottom_right = Shape.create(type=PrimitiveShape.CUBOID,
-                      				size=[0.05, rand/2, 2.0],
-                      				color=[0.8, 0.8, 0.8],
-		      						position=[rand/2-(rand/3), 2-(0.75*rand), 1.0],
-                      				static=True, respondable=False)
+                      size=[0.05, rand/2, HEIGHT],
+                      color=[0.8, 0.8, 0.8],
+		      position=[rand/2-(rand/3), 2-(0.75*rand), HEIGHT/2],
+                      static=True, respondable=True)
 	bottom = Shape.create(type=PrimitiveShape.CUBOID,
-                      	size=[rand/3, 0.05, 2.0],
-                      	color=[0.8, 0.8, 0.8],
-		      			position=[0.0, 2-rand, 1.0],
-                      	static=True, respondable=False)
+                      size=[rand/3, 0.05, HEIGHT],
+                      color=[0.8, 0.8, 0.8],
+		      position=[0.0, 2-rand, HEIGHT/2],
+                      static=True, respondable=True)
+	robot_set_position()
 	data = {
 		'shape': 'troom',
-    	'point1': (rand/2, 2.0),
-    	'point2': (-rand/2, 2.0),
-    	'point3': (-rand/2, 2.0-rand/2),
-    	'point4': (-rand/2+(rand/3), 2.0-rand/2),
-    	'point5': (-rand/2+rand/3, 2.0-rand),
-    	'point6': (rand/2-rand/3, 2.0-rand),
-    	'point7': (rand/2-rand/3, 2.0-rand/2),
-    	'point8': (rand/2, 2-rand/2)
+    		'point1': (rand/2, 2.0),
+    		'point2': (-rand/2, 2.0),
+    		'point3': (-rand/2, 2.0-rand/2),
+    		'point4': (-rand/2+(rand/3), 2.0-rand/2),
+    		'point5': (-rand/2+rand/3, 2.0-rand),
+    		'point6': (rand/2-rand/3, 2.0-rand),
+    		'point7': (rand/2-rand/3, 2.0-rand/2),
+    		'point8': (rand/2, 2-rand/2)
 	}
 	with open('data.json', 'a') as f:
 		json.dump(data, f)
-	#shape = [(rand/2, 2.0), (-rand/2, 2.0), (-rand/2, 2.0-(rand/2)), 
-			#(-rand/2+(rand/3), 2.0-(rand/2)), (-rand/2+rand/3, 2.0-rand),
-			#(rand/2-rand/3, 2.0-rand), ((rand/2-rand/3), 2.0-rand/2), 
-			#(rand/2, 2-rand/2)]
-	print("rand: " , rand)
-	print(data)
-	
+
 def l_room_generation():
 	#generate random to insert into the shape generation
-	rand = randint(30,80)/10
-	#print("lroom: " + rand+ "")
+	rand = randint(40,80)/10
 	top = Shape.create(type=PrimitiveShape.CUBOID,
-                     size=[rand/2, 0.05, 2.0],
+                     size=[rand/2, 0.05, HEIGHT],
                      color=[0.8, 0.8, 0.8],
-		      		 position=[0.0, 2.0, 1.0],
-                     static=True, respondable=False)
+		     position=[0.0, 2.0, HEIGHT/2],
+                     static=True, respondable=True)
 	middle_left = Shape.create(type=PrimitiveShape.CUBOID,
-	              				size=[0.05, rand, 2.0],
-                      			color=[0.8, 0.8, 0.8],
-		      					position=[-(rand/4), 2-(rand/2), 1.0],
-                      			static=True, respondable=False)
+	             size=[0.05, rand, HEIGHT],
+                     color=[0.8, 0.8, 0.8],
+		     position=[-(rand/4), 2-(rand/2), HEIGHT/2],
+                     static=True, respondable=True)
 	middle_right = Shape.create(type=PrimitiveShape.CUBOID,
-                      				size=[0.05, rand/2, 2.0],
-                      				color=[0.8, 0.8, 0.8],
-		      						position=[rand/4,2-(rand/4), 1.0],
-                      				static=True, respondable=False)
+                     size=[0.05, rand/2, HEIGHT],
+                     color=[0.8, 0.8, 0.8],
+		     position=[rand/4,2-(rand/4), HEIGHT/2],
+                     static=True, respondable=True)
 	bottom_right = Shape.create(type=PrimitiveShape.CUBOID,
-                      				size=[rand/2, 0.05, 2.0],
-                      				color=[0.8, 0.8, 0.8],
-		      						position=[rand/2, 2-(rand/2), 1.0],
-                      				static=True, respondable=False)
+                     size=[rand/2, 0.05, HEIGHT],
+                     color=[0.8, 0.8, 0.8],
+		     position=[rand/2, 2-(rand/2), HEIGHT/2],
+                     static=True, respondable=True)
 	right = Shape.create(type=PrimitiveShape.CUBOID,
-                       	size=[0.05, rand/2, 2.0],
-                       	color=[0.8, 0.8, 0.8],
-		      			position=[(3/4*rand), 2-(3/4*rand), 1.0],
-                       	static=True, respondable=False)
+                     size=[0.05, rand/2, HEIGHT],
+                     color=[0.8, 0.8, 0.8],
+		     position=[(3/4*rand), 2-(3/4*rand), HEIGHT/2],
+                     static=True, respondable=True)
 	bottom = Shape.create(type=PrimitiveShape.CUBOID,
-	              		size=[rand, 0.05, 2.0],
-                        color=[0.8, 0.8, 0.8],
-		      			position=[rand/4, 2-rand, 1.0],
-                        static=True, respondable=False)
+	             size=[rand, 0.05, HEIGHT],
+                     color=[0.8, 0.8, 0.8],
+		     position=[rand/4, 2-rand, HEIGHT/2],
+                     static=True, respondable=True)
+	robot_set_position()
     #points recorded starting from top right corner- anti clockwise round
-	#data = {}
-  #data['shape'] = []  	
 	data = {
 		'shape': 'lroom',
-    	'point1': (rand/4, 2.0),
-    	'point2': (-rand/4, 2.0),
-    	'point3': (-rand/4, 2-rand),
-    	'point4': (-rand/4 + rand, 2.0-rand),
-    	'point5': (-rand/4+rand, 2-rand/2),
-    	'point6': (rand/4, 2-rand/2)
+    		'point1': (rand/4, 2.0),
+    		'point2': (-rand/4, 2.0),
+    		'point3': (-rand/4, 2-rand),
+    		'point4': (-rand/4 + rand, 2.0-rand),
+    		'point5': (-rand/4+rand, 2-rand/2),
+    		'point6': (rand/4, 2-rand/2)
 	}
 	with open('data.json', 'a') as f:
 		json.dump(data, f)
-	print(data)
-	#shape = [(rand/4, 2.0), (-rand/4, 2.0), (-rand/4, 2-rand), (-rand/4 + rand, 2.0-rand),
-	#		(-rand/4+rand, 2-(rand/2)), (rand/4, 2-rand/2)]
-	print("rand is: " , rand)
-	#print(shape)
 
 #Selects the "type" of room to create at random
 random_value = randint(1,3)
-print(random_value)
 if random_value == 1:
 	rectangular_room_generation()
-	#myData={room}
-	#sim.writeCustomDataBlock(sim.handle_scene,"myTag",sim.packTable(myData))
 elif random_value == 2:
 	t_room_generation()
 else:
 	l_room_generation()
 	
+#method for closing the programme properly
 def getch():
 	fd = sys.stdin.fileno()
 	oldterm = termios.tcgetattr(fd)
@@ -214,7 +225,8 @@ def getch():
 	if len(c)>0:
 		return ord(c)
 	return -1
-
+	
+#main event loop
 while True:
 	got=getch()
 	if got == 27:
@@ -223,9 +235,6 @@ while True:
 		pr.shutdown()
 	else:
 		pr.step()
+		joystick_movement()
 	time.sleep(0.01)
-#	#done = True
-#print('Done ...')
-#input('Press enter to finish ...')
-#pr.stop()  # Stop the simulation
-#pr.shutdown()  # Close the application
+
